8326b921f6c83700d7a213a65b6edc246fa4e78d
Vaishnaveegupta21 <vaishu.gupta21@gmail.com>
2021-10-09 14:20:50 +0530

    changes in cart

--- src/app/component/cart/cart.component.ts
+++ src/app/component/cart/cart.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-cart',
@@ -6,10 +7,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
-
-  constructor() { }
+  public products :any = [];
+  public grandTotal !: number ;
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.cartService.getProduct()
+      .subscribe(res=>{
+        this.products=res;
+        this.grandTotal=this.cartService.getTotalPrice();
+
+      })
+     
+  }
+  removeItem(item : any){
+    this.cartService.removeCartItem(item);
+  }
+  emptycart(){
+    this.cartService.removeAllCart();
   }
 
 }
--- src/app/component/products/products.component.ts
+++ src/app/component/products/products.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ApiComponent } from 'src/app/service/api/api.component';
+import { CartService } from 'src/app/service/cart.service';
 
 @Component({
   selector: 'app-products',
@@ -8,14 +9,20 @@ import { ApiComponent } from 'src/app/service/api/api.component';
 })
 export class ProductsComponent implements OnInit {
   public productList : any ;
-  constructor(private api : ApiComponent) { }
+  constructor(private api : ApiComponent, private cartService : CartService) { }
 
   ngOnInit(): void {
     this.api.getProduct()
     .subscribe(res => {
       this.productList=res;
+      this.productList.forEach((a:any) => {
+        Object.assign(a, {quantity:1, total:a.price});
+      });
 
     })
+  }
+  addtocart(item: any){
+    this.cartService.addtoCart(item);
   }
 
 }
